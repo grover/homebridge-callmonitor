c00314518a6269fef496bbbd2ed59bca45c73c8f
bgewehr <bernd@gewehr.net>
2021-02-28 20:24:14 +0100

    Added incomingLines Parameter - made it optional

--- src/CallMonitorAccessory.js
+++ src/CallMonitorAccessory.js
@@ -206,7 +206,7 @@ class CallMonitorAccessory {
     // CALL -> CONNECT -> DISCONNECT
     // RING -> CONNECT -> DISCONNECT
     //
-    if (this._incomingLines.indexOf(data[3]) || this._incomingLines === ["*"]) {
+    if (this._incomingLines.indexOf(data[3]) || this._incomingLines[0] === "*" || !this._incomingLines) {
       if (data[1] === 'CALL' || data[1] === 'RING') {
         this._activeConnections.push({
           id: data[2],
